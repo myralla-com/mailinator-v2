cc23ae4f4ebc2e48320ab561bc4b19854f432283
CJ-Felicitas <cedrick@apollo.com.ph>
2024-12-22 20:35:10 +0800

    initial commit

---
+++ src/main/java/com/myralla/mailinator/MailinatorApplication.java
@@ -0,0 +1,13 @@
+package com.myralla.mailinator;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MailinatorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MailinatorApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/myralla/mailinator/MailinatorApplicationTests.java
@@ -0,0 +1,13 @@
+package com.myralla.mailinator;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MailinatorApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
